fd1cff7a8368150f3f1239bc52d1d17c152187c1
2777maxim <2777maxim@mail.ru>
2024-11-16 05:06:29 +0300

    Переработана архитектура и добавлена новая логика (с лайками и друзьями)

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,10 +6,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
-    Film addFilm(Film film);               // Добавить новый фильм
-    Film updateFilm(Film film);            // Обновить существующий фильм
-    Optional<Film> getFilmById(Long id);   // Получить фильм по его ID
-    void deleteFilm(Long id);              // Удалить фильм по ID
-    List<Film> getAllFilms();              // Получить все фильмы
+    // Добавить новый фильм
+    Film addFilm(Film film);
+    // Обновить существующий фильм
+    Film updateFilm(Film film);
+    // Получить фильм по его ID
+    Optional<Film> getFilmById(Long id);
+    // Удалить фильм по ID
+    void deleteFilm(Long id);
+    // Получить все фильмы
+    List<Film> getAllFilms();
 }
 
