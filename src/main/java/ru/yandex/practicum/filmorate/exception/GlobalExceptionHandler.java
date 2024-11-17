0b5db51d18fd159601c518eb55d9fbecd31bf2c3
2777maxim <2777maxim@mail.ru>
2024-11-17 05:56:48 +0300

    Добавлено @Slf4j

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,19 +12,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-    // Обработчик исключения ResourceNotFoundException.
     // Возвращает объект ErrorResponse и HTTP статус 404 (NOT FOUND).
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(ResourceNotFoundException e) {
+        log.error("Ошибка ResourceNotFoundException: {}", e.getMessage());
         return new ResponseEntity<>(e.getErrorResponse(), HttpStatus.NOT_FOUND);
     }
 
-    // Обработчик исключения RuntimeException.
     // Возвращает сообщение об ошибке и HTTP статус 400 (BAD REQUEST).
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        log.error("Ошибка RuntimeException: {}", e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
@@ -31,7 +33,7 @@ public class GlobalExceptionHandler {
     // Возвращает карту с полями и ошибками валидации и HTTP статус 400 (BAD REQUEST).
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        // Создаем карту для хранения ошибок.
+        // Создаем Map для хранения ошибок.
         Map<String, String> errors = new HashMap<>();
 
         // Проходим по всем ошибкам и добавляем их в карту с указанием имени поля и сообщения об ошибке.
@@ -41,6 +43,8 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
 
+        log.error("Ошибки валидации: {}", errors);
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
@@ -48,9 +52,11 @@ public class GlobalExceptionHandler {
     // Возвращает сообщение "Internal Server Error" и HTTP статус 500 (INTERNAL SERVER ERROR).
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleAllExceptions(Exception e) {
+        log.error("Внутренняя ошибка сервера: ", e);
         return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
 
 
 
