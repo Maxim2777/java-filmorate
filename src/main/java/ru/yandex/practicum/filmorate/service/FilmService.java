0058ca691f6e055a99fdd1327b9572561bddd528
2777maxim <2777maxim@mail.ru>
2024-11-17 04:12:19 +0300

    При удалении и добавлении лайка добавлена проверка id пользователя. Также добавлены комментарии с пояснениями к методам.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.ResourceNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,22 +13,38 @@ import java.util.stream.Collectors;
 @Service
 public class FilmService {
     private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
 
     @Autowired
-    public FilmService(FilmStorage filmStorage) {
+    public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
         this.filmStorage = filmStorage;
+        this.userStorage = userStorage;
     }
 
     public void addLike(Long filmId, Long userId) {
+        // Проверка наличия фильма
         Film film = filmStorage.getFilmById(filmId)
                 .orElseThrow(() -> new ResourceNotFoundException("Фильм с id " + filmId + " не найден"));
+
+        // Проверка наличия пользователя
+        userStorage.getUserById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Пользователь с id " + userId + " не найден"));
+
+        // Добавление лайка
         film.getLikes().add(userId);
         filmStorage.updateFilm(film);
     }
 
     public void removeLike(Long filmId, Long userId) {
+        // Проверка наличия фильма
         Film film = filmStorage.getFilmById(filmId)
                 .orElseThrow(() -> new ResourceNotFoundException("Фильм с id " + filmId + " не найден"));
+
+        // Проверка наличия пользователя
+        userStorage.getUserById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Пользователь с id " + userId + " не найден"));
+
+        // Удаление лайка
         film.getLikes().remove(userId);
         filmStorage.updateFilm(film);
     }
@@ -40,6 +57,7 @@ public class FilmService {
                 .collect(Collectors.toList());
     }
 }
+
 
 
 
