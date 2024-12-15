6e2effaa2039ef03a80af7a0a1f7db4b57fb2f6a
2777maxim <2777maxim@mail.ru>
2024-12-15 09:04:56 +0300

    Изменен принцип получения сгенерированного id

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorage.java
@@ -7,8 +7,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.MpaRating;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
@@ -29,13 +33,34 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public Film addFilm(Film film) {
         String sql = "INSERT INTO films (name, description, release_date, duration) VALUES (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, film.getName(), film.getDescription(), film.getReleaseDate(), film.getDuration());
+        String[] generatedColumns = {"film_id"}; // Указываем столбец сгенерированного ключа
 
-        Long filmId = jdbcTemplate.queryForObject(
-                "SELECT film_id FROM films WHERE name = ? AND description = ? AND release_date = ? AND duration = ?",
-                Long.class, film.getName(), film.getDescription(), film.getReleaseDate(), film.getDuration());
-        film.setId(filmId);
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql, generatedColumns)) {
 
+            // Устанавливаем параметры запроса
+            statement.setString(1, film.getName());
+            statement.setString(2, film.getDescription());
+            statement.setDate(3, Date.valueOf(film.getReleaseDate()));
+            statement.setInt(4, film.getDuration());
+
+            // Выполняем запрос
+            statement.executeUpdate();
+
+            // Получаем сгенерированный идентификатор
+            try (ResultSet rs = statement.getGeneratedKeys()) {
+                if (rs.next()) {
+                    film.setId(rs.getLong(1)); // Устанавливаем сгенерированный ID в объект Film
+                } else {
+                    throw new SQLException("Не удалось получить сгенерированный идентификатор для фильма");
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка при добавлении фильма: " + e.getMessage(), e);
+        }
+
+        // Добавляем MPA и жанры
         addMpaToFilm(film);
         addGenresToFilm(film);
 
@@ -74,7 +99,6 @@ public class FilmDbStorage implements FilmStorage {
         return jdbcTemplate.query(sql, this::mapRowToFilm, id).stream().findFirst();
     }
 
-
     public void addLike(Long filmId, Long userId) {
         String sql = "INSERT INTO likes (film_id, user_id) VALUES (?, ?)";
         jdbcTemplate.update(sql, filmId, userId);
@@ -112,6 +136,7 @@ public class FilmDbStorage implements FilmStorage {
     }
 
     private Film mapRowToFilm(ResultSet rs, int rowNum) throws SQLException {
+        // Создаём объект Film
         Film film = new Film();
         film.setId(rs.getLong("film_id"));
         film.setName(rs.getString("name"));
@@ -119,11 +144,37 @@ public class FilmDbStorage implements FilmStorage {
         film.setReleaseDate(rs.getDate("release_date").toLocalDate());
         film.setDuration(rs.getInt("duration"));
 
-        // Запрос для получения лайков
+        // Получаем лайки
         String likesSql = "SELECT user_id FROM likes WHERE film_id = ?";
         List<Long> likes = jdbcTemplate.queryForList(likesSql, Long.class, film.getId());
         film.setLikes(new HashSet<>(likes));
 
+        // Получаем MPA
+        String mpaSql = "SELECT m.mpa_rating_id, m.name " +
+                "FROM mpa_rating m " +
+                "JOIN film_mpa_rating fmr ON m.mpa_rating_id = fmr.mpa_rating_id " +
+                "WHERE fmr.film_id = ?";
+        jdbcTemplate.query(mpaSql, (rsMpa) -> {
+            MpaRating mpaRating = new MpaRating();
+            mpaRating.setId(rsMpa.getLong("mpa_rating_id"));
+            mpaRating.setName(rsMpa.getString("name"));
+            film.setMpa(mpaRating);
+        }, film.getId());
+
+        // Получаем жанры
+        String genresSql = "SELECT g.genre_id, g.name " +
+                "FROM genre g " +
+                "JOIN film_genre fg ON g.genre_id = fg.genre_id " +
+                "WHERE fg.film_id = ?";
+        List<Genre> genres = jdbcTemplate.query(genresSql, (rsGenre, rowNumGenre) -> {
+            Genre genre = new Genre();
+            genre.setId(rsGenre.getLong("genre_id"));
+            genre.setName(rsGenre.getString("name"));
+            return genre;
+        }, film.getId());
+        film.setGenres(new HashSet<>(genres));
+
         return film;
     }
+
 }
